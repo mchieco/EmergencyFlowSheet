a2504db9da1ffcc911880752aae39866d9ed6dca
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2018-10-16 13:52:04 -0400

    new stuff

--- DataInput.js
+++ DataInput.js
@@ -5,7 +5,15 @@ function sendData() {
 function textBoxKeyPress(e) {
   e = e || window.event;
   if (e.keyCode != 13) return;
-  
+
   //move info to table
+  var textBox = document.getElementById('text_box');
+  //removes text and saves it to var
+  var text = textBox.value.replace(/\n/, '');
+  textBox.value = "";
+  console.log(text);
   sendData();
+
+  //keeps from submitting form
+  return false;
 }
