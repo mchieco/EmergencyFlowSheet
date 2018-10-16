7fd7415fd685754e3df7f6df372c82fa00858715
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-10-16 14:22:22 -0400

    Updated Staff Page

---
+++ staffPage.js
@@ -0,0 +1,15 @@
+
+var radioCompleted = false;
+
+//gets elements
+var reason_btn = document.getElementById("reason");
+var notified_btn = document.getElementById("family_notified");
+var notified_event = document.getElementById("family_notified_event");
+var notified_outcome = document.getElementById("family_notified_outcome");
+
+var submit = document.getElementById("submit_btn");
+var edit = document.getElementById("edit_btn");
+
+function editFunction(){
+  window.location = 'DataInput.html';
+}
