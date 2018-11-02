c8468dbbcbb1e96158a55ceeab48332ace40cbfe
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2018-11-02 13:35:45 -0400

    getFunciton

--- app/resources/static/js/ConfirmPage.js
+++ app/resources/static/js/ConfirmPage.js
@@ -1,21 +1,9 @@
-function parseURLParams(url) {
-    var queryStart = url.indexOf("?") + 1,
-        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-        query = url.slice(queryStart, queryEnd - 1),
-        pairs = query.replace(/\+/g, " ").split("&"),
-        parms = {}, i, n, v, nv;
-
-    if (query === url || query === "") return;
-
-    for (i = 0; i < pairs.length; i++) {
-        nv = pairs[i].split("=", 2);
-        n = decodeURIComponent(nv[0]);
-        v = decodeURIComponent(nv[1]);
-
-        if (!parms.hasOwnProperty(n)) parms[n] = [];
-        parms[n].push(nv.length === 2 ? v : null);
+function getParams(){
+    var params = {};
+    var param_array = window.location.href.split('?')[1].split('&');
+    for(var i in param_array){
+        x = param_array[i].split('=');
+        params[x[0]] = x[1];
     }
-    return parms;
-}
-
-var parms = parseURLParams();
+    return params;
+};
